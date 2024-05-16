3e4ae2a436d787dc3ee3588c920f6675a236f340
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-05-15 22:04:34 -0300

    feat: add renderThemeItem

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Theme Selector
 
+Aplicação web criada com:
+
 - HTML
 - CSS
 - JavaScript
+
+## TODO
+
+- [x] Utilize JS Vanilla e CSS nativo para a estrutura HTML responsiva
+- [x] Deve exibir uma lista com todos os temas do JSON de mock fornecido
+- [ ] Cada tema deve ter uma visualização em miniatura
+- [ ] Qualquer tema que não esteja aplicado no momento pode ser removido
+- [ ] Ao clicar em um tema, ele deve ser aplicado
+-
--- src/utils/render-theme-list.js
+++ src/utils/render-theme-list.js
@@ -1,19 +1,39 @@
 import { themes } from "../mock/themes.js";
 
+console.log(themes[0].colors);
+
+function renderButton(colors) {
+  const button = document.createElement('button')
+  button.style.backgroundColor = colors.primary
+  button.textContent = 'Selecionar tema'
+
+  return button
+}
+
+function renderThemeItem(theme) {
+  const themeWrapper = document.createElement("li");
+  themeWrapper.style.border = `2px solid ${theme.colors.primary}`;
+  themeWrapper.style.padding = "1.5rem";
+
+  const title = document.createElement("h3");
+  title.style.marginBottom = '1rem'
+  title.textContent = theme.name;
+  
+  const button = renderButton(theme.colors)
+  
+  themeWrapper.appendChild(title);
+  themeWrapper.appendChild(button);
+
+  return themeWrapper;
+}
+
 export function renderThemeList() {
   const themeListElement = document.querySelector("#theme-list");
 
   themeListElement.innerHTML = "";
 
   themes.forEach((theme) => {
-    const li = document.createElement("li");
-
-    li.style.backgroundColor = theme.colors.primary
-    li.style.fontWeight = 700
-
-    li.textContent = theme.name;
-    themeListElement.appendChild(li);
+    const themeItem = renderThemeItem(theme);
+    themeListElement.appendChild(themeItem);
   });
-
-  console.log(themeListElement);
 }
